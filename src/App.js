0cadd02cb0e0c718724e96ebde08d32e1676f7eb
AlexMagar <lx.mgr07@gmail.com>
2023-07-16 14:26:34 +1000

    cms admin front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import SignIn from './pages/signin-signup/SignIn'
+import SignUp from './pages/signin-signup/SignUp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+
+        {/* Private router */}
+        <Route path='/new-admin' element={<SignUp />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
