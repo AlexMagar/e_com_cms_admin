d359180f5fe5fb8222c4c6dadd03a739a94b4a6b
AlexMagar <lx.mgr07@gmail.com>
2023-07-17 22:27:44 +1000

    move content to div section

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-        <div className="bg-dark text-light p-5 text-center"></div>
+  <div className="bg-dark text-light p-5 text-center">
         &copy; copy right all reserved || e-com store
     </div>
   )
