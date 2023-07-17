31bb3ba9f32dd7bbc9848bbcb318320234bc0972
AlexMagar <lx.mgr07@gmail.com>
2023-07-17 22:28:19 +1000

    added signout link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,21 +2,26 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 
 export const Header = () => {
+
+  const navigate = useNavigate()
+  const handleOnSignOut = () =>{
+    navigate("/");
+  }
   return (
     <div> 
-    <Navbar expand="md" variant="dark" className="bg-body-tertiary">
+    <Navbar expand="md" variant="dark" className="bg-dark">
     <Container>
       <Link to="/" className='navbar-brand'>E-Store</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Link to="/" className="nav-link">SignIn</Link>
+          <Link to="/login" className="nav-link">SignIn</Link>
           <Link to="/new-admin" className="nav-link">SignUp</Link>
-
+          <Link to="/" className="nav-link" onClick={handleOnSignOut}>SignOut</Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
