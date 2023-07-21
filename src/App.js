5d819b95b5b9f6403d0352749accaa129f7802ea
AlexMagar <lx.mgr07@gmail.com>
2023-07-21 22:36:27 +1000

    dispatching fuction

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import { ToastContainer } from "react-toastify";
 import {SignIn} from './pages/signin-signup/SignIn'
 import {SignUp} from './pages/signin-signup/SignUp'
 import { Home } from './pages/home/Home';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCmsAction } from "./pages/cms/cmsAction";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  //fetch the cms
+  useEffect(() =>{
+    dispatch(fetchCmsAction());
+  }, [dispatch]);
+
   return (
     <div>
       <Routes>
