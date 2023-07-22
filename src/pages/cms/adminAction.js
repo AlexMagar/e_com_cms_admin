9f618cc035bca1ac832e73e906bf6e6e35357d5d
AlexMagar <lx.mgr07@gmail.com>
2023-07-22 20:26:27 +1000

    created new action to add new admin

---
+++ src/pages/cms/adminAction.js
@@ -0,0 +1,9 @@
+import {toast} from "react-toastify"
+import { postNewAdmin } from '../../helper/axios';
+
+export const createNewAdminAction = async (obj) => {
+    
+    const {status, message} = await postNewAdmin(obj)
+
+    toast[status](message);
+}
