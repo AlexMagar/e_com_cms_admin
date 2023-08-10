0c1964acda55e36a308c4ba383fea7f1b196add3
AlexMagar <lx.mgr07@gmail.com>
2023-08-10 22:09:05 +1000

    payment

--- src/pages/payment-option/poAction.js
+++ src/pages/payment-option/poAction.js
@@ -1,6 +1,7 @@
 import { toast } from "react-toastify"
-import { gettNewPOs, postNewPO } from "../../helper/axios"
+import { deletePO, gettNewPOs, postNewPO, updateNewPOs } from "../../helper/axios"
 import { setPaymentOptions } from "./poSlice";
+import { setModalShow } from "../../system/systemSlice";
 
 export const addNewPOAction = data => async (dispatch) =>{
     const { status, message } = await postNewPO(data)
@@ -17,4 +18,34 @@ export const getOPsAction = () => async (dispatch) =>{
     const {status, result} = await gettNewPOs()
 
     status === "success" && dispatch(setPaymentOptions(result))
+}
+
+export const updatePOsAction = (data) => async (dispatch) =>{
+    const pending = await updateNewPOs(data)
+
+    toast.promise(pending, {
+        pending: "Please wait....."
+    })
+
+    const { status, message} = await pending
+
+    toast[status](message)
+
+    status === "success" && dispatch(getOPsAction())
+}
+
+
+export const deletePOsAction = (_id) => async (dispatch) =>{
+    const pending = await deletePO(_id);
+
+    toast.promise(pending, {
+        pending: "Please wait....."
+    })
+
+    const { status, message} = await pending
+
+    toast[status](message)
+
+    status === "success" && dispatch(getOPsAction())
+    dispatch(setModalShow(false))
 }
