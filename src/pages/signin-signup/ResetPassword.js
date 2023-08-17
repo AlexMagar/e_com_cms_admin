56aa6ec44568bb9126d4d13116b0b8d49045bbd3
AlexMagar <lx.mgr07@gmail.com>
2023-08-17 22:50:00 +1000

    git commit -m "processResetPassAPI added for password OTP"

--- src/pages/signin-signup/ResetPassword.js
+++ src/pages/signin-signup/ResetPassword.js
@@ -4,11 +4,14 @@ import { Footer } from "../../components/layout/Footer";
 import { useSelector } from 'react-redux';
 import { requestPassOTP } from '../../helper/axios';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
 
     const [form, setForm] = useState('otp')
     const[resp, setResp] = useState({})
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("")
 
     const handleOnOtpRequest = async (email) =>{
 
@@ -26,15 +29,48 @@ const ResetPassword = () => {
         setForm(result)
     }
 
+    const processResetPassAPI = async (obj) => {
+        const pending = resetPass({ ...obj, email });
+        toast.promise(pending, {
+          pending: "Please wait...",
+        });
+    
+        const { status, message } = await pending;
+        toast[status](message);
+    
+        status === "success" && navigate("/");
+      };
+    
+      const forms = {
+        otp: <PasswordOTP handleOnOtpRequest={handleOnOtpRequest} />,
+        reset: (
+          <PasswordReset
+            setForm={setForm}
+            processResetPassAPI={processResetPassAPI}
+          />
+        ),
+      };
+
   return (
-     <>
-       <Header />
-            <main className='main pt-5'>
-                <div className="d-flex reset-pass">
-                </div>
-            </main>
-       <Footer />
-     </>
+    <>
+    <Header />
+    <main className="main pt-5">
+      {resp.message && (
+        <Container>
+          <Alert variant={resp.status === "success" ? "success" : "danger"}>
+            {resp.message}
+          </Alert>
+        </Container>
+      )}
+      <div className="d-flex reset-pass">
+        {/* requeset opt form */}
+        {forms[form]}
+
+        {/* rest password form  */}
+      </div>
+    </main>
+    <Footer />;
+  </>
     )
 }
 
