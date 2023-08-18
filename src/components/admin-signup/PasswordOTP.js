af8ec6c441a99b56b3abd80c02eed45cb43b5d07
AlexMagar <lx.mgr07@gmail.com>
2023-08-18 22:56:46 +1000

    added useRef

--- src/components/admin-signup/PasswordOTP.js
+++ src/components/admin-signup/PasswordOTP.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { CustomInput } from '../custom-input/CustomInput'
 
 export const PasswordOTP = () => {
+
+  const emailRef = useRef("")
+
+  const handleOnOTPRequest = () =>{
+    const {value} = emailRef.current;
+    if(value){
+      handleOnOTPRequest(value)
+    }
+  }
+
+
   return (
     <Form>
         <h3>Request OTP</h3>
         <hr />
-        <CustomInput label="email" />
+        <CustomInput label="email" someRef={emailRef} placeholder="sam@email.com"/>
         <div className="d-grid mt-3">
-            <Button variant='dark'>Request OTP</Button>
+            <Button variant='dark' onClick={handleOnOTPRequest}>Request OTP</Button>
         </div>
     </Form>
   )
