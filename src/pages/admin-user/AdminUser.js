3fd2adf00a5ef3ef644c5fceffb3c067f3f21408
AlexMagar <lx.mgr07@gmail.com>
2023-08-26 23:06:20 +1000

    New admin Table added

--- src/pages/admin-user/AdminUser.js
+++ src/pages/admin-user/AdminUser.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { AdminLayout } from '../../components/layout/AdminLayout'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
+import { AdminTable } from "../../components/AdminUser/AdminTable";
 
 export const AdminUser = () => {
+
   return (
     <div>
         <AdminLayout title="AdminUser">
@@ -11,6 +13,8 @@ export const AdminUser = () => {
             <Link to="/new-admin" className='nav-link'>
               <Button variant='warning'>Add New Admin</Button>
             </Link>
+            <hr />
+            <AdminTable />
           </div>
         </AdminLayout>
     </div>
