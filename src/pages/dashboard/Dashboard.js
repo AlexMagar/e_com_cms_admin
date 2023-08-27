e18c18339b4e74de812f71e46fbb51c669f4b29f
AlexMagar <lx.mgr07@gmail.com>
2023-08-27 18:35:56 +1000

    charts

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,55 @@
 import React from 'react'
 import { AdminLayout } from '../../components/layout/AdminLayout'
+import { LineChart } from "recharts";
 
 export const Dashboard = () => {
+
+  const data = [
+    {
+      name: 'January',
+      uv: 1000,
+      pv: 2000,
+    },
+    {
+      name: 'February',
+      uv: 2000,
+      pv: 3000,
+    },
+    {
+      name: 'March',
+      uv: 3000,
+      pv: 4000,
+    },
+    {
+      name: 'April',
+      uv: 4000,
+      pv: 5000,
+    },
+    {
+      name: 'May',
+      uv: 5000,
+      pv: 6000,
+    },
+  ];
+
+  
   return (
     <div>
         <AdminLayout title="Dashboard">Dashboard</AdminLayout>
+        <div>
+        <LineChart
+      data={data}
+      width={500}
+      height={300}
+      xAxis={20}
+      yAxis={20}
+      className="chart-container"
+      style={{
+        background: "blue",
+        color: "white",
+      }}
+    />
+        </div>
     </div>
   )
 }
