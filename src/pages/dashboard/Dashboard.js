c2a27a04d2639651a77be0253a8cfe39b1f6fe1b
AlexMagar <lx.mgr07@gmail.com>
2023-08-28 23:05:57 +1000

    pie charts

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,49 +1,53 @@
-import React from 'react'
-import { AdminLayout } from '../../components/layout/AdminLayout'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+
+import React from "react";
+import { PieChart, Pie, Legend, Tooltip } from "recharts";
+import { AdminLayout } from "../../components/layout/AdminLayout";
+
+const data01 = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+  { name: "Group E", value: 278 },
+  { name: "Group F", value: 189 }
+];
+
+const data02 = [
+  { name: "Group A", value: 2400 },
+  { name: "Group B", value: 4567 },
+  { name: "Group C", value: 1398 },
+  { name: "Group D", value: 9800 },
+  { name: "Group E", value: 3908 },
+  { name: "Group F", value: 4800 }
+];
 
 export const Dashboard = () => {
-
-  const data = [
-    {
-      name: 'January',
-      uv: 1000,
-      pv: 2000,
-    },
-    {
-      name: 'February',
-      uv: 2000,
-      pv: 3000,
-    },
-    {
-      name: 'March',
-      uv: 3000,
-      pv: 4000,
-    },
-    {
-      name: 'April',
-      uv: 4000,
-      pv: 5000,
-    },
-    {
-      name: 'May',
-      uv: 5000,
-      pv: 6000,
-    },
-  ];
-
   return (
-    <div>
-        <AdminLayout title="Dashboard">
-        <div className='App p-5 b-solid'>
-        <LineChart width={500} height={300} data={data}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          <CartesianGrid stroke='#ccc' />
-          <XAxis dataKey="name" />
-          <YAxis / >
-        </LineChart>
-        </div>
-        </AdminLayout>
+    <AdminLayout title="Dashboard">
+      <div className="border p-3 shadow-lg rounded">
+    <PieChart width={600} height={400}>
+      <Pie
+        dataKey="value"
+        isAnimationActive={false}
+        data={data01}
+        cx={200}
+        cy={200}
+        outerRadius={80}
+        fill="#8884d8"
+        label
+      />
+      <Pie
+        dataKey="value"
+        data={data02}
+        cx={500}
+        cy={200}
+        innerRadius={40}
+        outerRadius={80}
+        fill="#82ca9d"
+      />
+      <Tooltip />
+    </PieChart>
     </div>
-  )
+    </AdminLayout>
+  );
 }
